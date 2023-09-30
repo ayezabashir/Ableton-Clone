30e4e07f160753bc94f25d3d14dd528c8605266c
Ayeza <ayezabashir442@gmail.com>
2023-10-01 00:03:41 +0500

    footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,6 +13,7 @@ const Footer = () => {
         { value: 'en', label: 'English' },
         { value: 'fr', label: 'Francais' },
         { value: 'es', label: 'Espanol' },
+        { value: 'de', label: 'Deutsch' },
     ];
     const changeHandler = value => {
         setValue(value)
@@ -28,59 +29,72 @@ const Footer = () => {
                         <div className="footer-row">
                             <h1>Ableton</h1>
                         </div>
-                        <div className="footer-row">
+                        <div className="footer-row a-flex">
                             <div className="footer-col">
-                                <a href="#">Register Live or Push <AiOutlineRight /> </a>
-                                <a href="#">About Ableton <AiOutlineRight /></a>
-                                <a href="#">Jobs <AiOutlineRight /> </a>
-                                <div className="social-media">
-                                    <div className="facebook">
+                                <a href="#" className='font-light'>Register Live or Push <AiOutlineRight /> </a>
+                                <a href="#" className='font-light'>About Ableton <AiOutlineRight /></a>
+                                <a href="#" className='font-light'>Jobs <AiOutlineRight /> </a>
+                                <div className="social-media a-flex">
+                                    <div className="facebook flex">
                                         <FaFacebook />
                                     </div>
-                                    <div className="twitter">
+                                    <div className="twitter flex">
                                         < FaTwitter />
                                     </div>
-                                    <div className="youtube">
+                                    <div className="youtube flex">
                                         <AiOutlineYoutube />
                                     </div>
-                                    <div className="insta">
+                                    <div className="insta flex">
                                         <AiOutlineInstagram />
                                     </div>
                                 </div>
                             </div>
                             <div className="footer-col">
-                                <h3>Education</h3>
-                                <a href="#">Offers for students and teachers <AiOutlineRight /> </a>
-                                <a href="#">Ableton for the Classroom <AiOutlineRight /></a>
-                                <a href="#">Ableton for Colleges and Universities <AiOutlineRight /> </a>
+                                <h3 className='font-light'>Education</h3>
+                                <a href="#" className='font-light'>Offers for students and teachers <AiOutlineRight /> </a>
+                                <a href="#" className='font-light'>Ableton for the Classroom <AiOutlineRight /></a>
+                                <a href="#" className='font-light'>Ableton for Colleges and Universities <AiOutlineRight /> </a>
                             </div>
                             <div className="footer-col">
-                                <h3>Sign up to our newsletter</h3>
-                                <p>Enter your email address to stay up to date with the latest offers, tutorials, downloads, surveys and more.</p>
+                                <h3 className='font-light'>Sign up to our newsletter</h3>
+                                <p className='font-light'>Enter your email address to stay up to date with the latest offers, tutorials, downloads, surveys and more.</p>
                                 <div className="signup-form">
                                     <input type="text" placeholder='Email Address' />
-                                    <button>Sign up</button>
+                                    <button className='font-bold'>Sign up</button>
                                 </div>
                             </div>
                         </div>
-                        <div className="footer-row">
-                            <h3>Community</h3>
-                            <a href="#">Find Ableton User Groups <AiOutlineRight /> </a>
-                            <a href="#">Find Certified Training <AiOutlineRight /></a>
-                            <a href="#">Become a Certified Trainer <AiOutlineRight /> </a>
-                        </div>
-                        <div className="footer-row">
-                            <h3>Language and Location</h3>
-                            <Select
-                                value={selectedOption}
-                                onChange={handleChange}
-                                options={langOptions}
-                            />
-                            <Select options={options} value={value} onChange={changeHandler} />
-                        </div>
-                        <div className="footer-row">
+                        <div className="footer-row a-flex">
                             <div className="footer-col">
-                                <ul>
+                                <h3 className='font-light'>Community</h3>
+                                <a href="#" className='font-light'>Find Ableton User Groups <AiOutlineRight /> </a>
+                                <a href="#" className='font-light'>Find Certified Training <AiOutlineRight /></a>
+                                <a href="#" className='font-light'>Become a Certified Trainer <AiOutlineRight /> </a>
+                            </div>
+                            <div className="footer-col">
+                                <h3 className='font-light'>Language and Location</h3>
+                                <div className='flex options'>
+                                    <Select
+                                        value={selectedOption}
+                                        onChange={handleChange}
+                                        options={langOptions}
+                                        className='select'
+                                        placeholder="Language"
+                                    />
+                                    <Select
+                                        options={options}
+                                        value={value}
+                                        onChange={changeHandler}
+                                        className='select'
+                                        placeholder="Country"
+                                    />
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex help">
+                            <div className="footer-col">
+                                <ul className='contact flex font-light'>
                                     <li>Contact Us</li>
                                     <li>Press Resource</li>
                                     <li>Legal Info</li>
@@ -89,7 +103,7 @@ const Footer = () => {
                                     <li>Imprint</li>
                                 </ul>
                             </div>
-                            <div className="footer-col">
+                            <div className="footer-col flex">
                                 <p>Made in Berlin</p>
                                 <div className="logo">
                                     <img src={logo} alt="" />
