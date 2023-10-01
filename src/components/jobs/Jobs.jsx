1827a1f148de770d951d6a9e9f3300369b00fbb7
Ayeza <ayezabashir442@gmail.com>
2023-10-01 21:21:34 +0500

    jobs component

--- src/components/jobs/Jobs.jsx
+++ src/components/jobs/Jobs.jsx
@@ -5,7 +5,20 @@ const Jobs = () => {
                 <header className="jobs-header">
                     <h1 className="headline">Jobs at Ableton</h1>
                 </header>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolorem nobis, quaerat veniam neque optio debitis illum doloremque facilis cum itaque eligendi harum accusantium id animi quo? Possimus, tempora corporis magnam rerum ut deleniti perspiciatis explicabo, odit quaerat, illo molestiae. Similique, minus sed architecto, aperiam cupiditate omnis quaerat, perspiciatis debitis repellendus iusto dignissimos distinctio culpa eius. Ducimus, doloribus. Nulla qui placeat perspiciatis voluptate ducimus et eos delectus! Quos natus, eius dolor est quaerat nisi perspiciatis odit rem hic ducimus vero ipsam alias quas porro suscipit dolores dicta? Est libero non eaque reprehenderit beatae. Enim eligendi nulla aperiam voluptatem, fuga nam vitae fugit culpa sequi explicabo, recusandae ullam iure numquam laudantium quod maiores at eos totam sapiente placeat excepturi maxime, neque perspiciatis! Sapiente, eos ipsum? Hic dolorum sequi similique esse quam rem corporis? Porro sit minus laudantium enim autem veritatis at.</p>
+                <div className="jobs-text">
+                    <div className="body-text">
+                        <h1>Working at Ableton means being a part of creating products that shape the future of music culture in a dynamic and diverse environment</h1>
+                        <p className="font-light">Here at Ableton, we come from a wide range of cultural and professional backgrounds. We work hard to foster an environment in which people can grow both personally and professionally. We believe that diverse groups of people make better teams. We’re committed to increasing diversity in our workplace and providing equal opportunity throughout our recruitment process. Because of this we ask that you do not include a picture in your application documents. Thank you very much for your understanding.</p>
+                        <p className="font-light">If you want to apply for one of our open positions, we would be happy to receive your application through our application form. Please upload all necessary documents there.</p>
+                    </div>
+                    <div className="job-dept">
+                        <h2 className="font-light">Commerce</h2>
+                        <p className="font-light">Group Accountant (d/f/m)</p>
+                        <small className="font-light">Ableton AG / Germany</small>
+                        <p className="font-light">Referendar:in für die Wahlstation bzw. als Teil der Anwaltsstation (d/w/m)</p>
+                        <small className="font-light">Ableton AG / Germany</small>
+                    </div>
+                </div>
             </div>
         </>
     )
