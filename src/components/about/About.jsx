a0187b779f2eef756e573a8e2ef47208a38581aa
Ayeza <ayezabashir442@gmail.com>
2023-10-02 19:56:18 +0500

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,13 @@
-import photo1 from '/photo-1.jpg'
-import photo2 from '/photo-2.jpg'
-import photo3 from '/photo-3.jpg'
-import photo4 from '/photo-4.jpg'
-import photo5 from '/photo-5.jpg'
+import photo1 from '/images/photo-1.jpg'
+import photo2 from '/images/photo-2.jpg'
+import photo3 from '/images/photo-3.jpg'
+import photo4 from '/images/photo-4.jpg'
+import photo5 from '/images/photo-5.jpg'
+import photo7 from '/images/photo-7.jpg'
+import photo8 from '/images/photo-8.jpg'
+import photo6 from '/images/photo-6-a.jpg'
+import poster from '/images/poster-meet-the-makers.avif'
+import { AiOutlineRight } from 'react-icons/ai'
 const About = () => {
     return (
         <>
@@ -34,6 +39,33 @@ const About = () => {
                     </div>
                     <div className="right">
                         <img src={photo5} alt="" className='pic2' />
+                    </div>
+                </div>
+                <div className="about-text">
+                    <h2>We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.</h2>
+                    <p>Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We’ve learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don’t always agree with each other, but opinion and debate are valued and openly encouraged.</p>
+                </div>
+                <div className="about-imgs3">
+                    <img src={poster} alt="" />
+                </div>
+                <div className="about-text">
+                    <h2>We’re passionate about what we do, but we’re equally passionate about improving who we are.</h2>
+                    <p>We work hard to foster an environment where people can grow both personally and professionally, and we strive to create a wealth of opportunities to learn from and with each other.</p>
+                    <p>Alongside an internal training program, employees are actively supported in acquiring new knowledge and skills, and coached on applying these in their daily work. In addition, staff-organized development and music salons are a chance to discuss new technologies, production techniques and best practices.</p>
+                </div>
+                <div className="about-imgs4">
+                    <img src={photo6} alt="" className="pic1" />
+                    <img src={photo7} alt="" className='pic2' />
+                </div>
+                <div className="about-text">
+                    <h2>We want our employees to love it here. Since we’re looking for exceptional talent from around the world, we will do everything we can to make your transition as easy as possible.</h2>
+                    <p>If you&apos;re joining us in Berlin, we&apos;ll help with relocation and paperwork. We’ll even provide you with free German or English lessons. Plus, working in Germany means you can expect comprehensive health insurance for you and your family, as well as generous maternity and paternity leave. Office hours are flexible, but it’s not all work; we have several company and team outings throughout the year as well as a variety of fun, informal small-group activities.</p>
+                </div>
+                <div className="about-jobs">
+                    <img src={photo8} alt="" />
+                    <div className="jobs-text">
+                        <p>We’re really proud of the work we’ve done so far. But there’s so much more to come. If you’d like to be a part of it, please join us.</p>
+                        <a href="#">See Latest Jobs <AiOutlineRight /> </a>
                     </div>
                 </div>
             </div>
