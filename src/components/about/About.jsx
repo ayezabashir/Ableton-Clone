0568683e6ae5992de87975aa0b8c532f02e051ea
Ayeza <ayezabashir442@gmail.com>
2023-10-02 20:26:38 +0500

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,12 +16,14 @@ const About = () => {
                     <h1 className="headline">Ableton</h1>
                 </div>
                 <div className="about-text">
-                    <h2>We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h2>
+                    <h2>We make <span className='blue'>Live, Push</span> and <span className='blue'>Link</span> — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h2>
                     <p>Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
                 </div>
                 <div className="about-imgs">
                     <img src={photo1} alt="" className="pic1" />
-                    <img src={photo2} alt="" className='pic2' />
+                    <div className="pic2">
+                        <img src={photo2} alt="" />
+                    </div>
                 </div>
                 <div className="about-text">
                     <h2>Making music isn’t easy. It takes time, effort, and learning. But when you’re in the flow, it’s incredibly rewarding.</h2>
